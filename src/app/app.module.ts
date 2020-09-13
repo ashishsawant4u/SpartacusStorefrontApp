0359c1484c6d9886f23cabe13525a6f18cfbbe63
ashishsawant4u <ashishsawant4u@gmail.com>
2020-09-13 12:18:20 +0530

    adding  @spartacus/schematics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { translations, translationChunksConfig } from '@spartacus/assets';
+import { B2cStorefrontModule } from '@spartacus/storefront';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,28 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://api.c39j2-walkersde1-d3-public.model-t.cc.commerce.ondemand.com/',
+          prefix: '/rest/v2/'
+        }
+      },
+      context: {
+        currency: ['USD'],
+        language: ['en'],
+        baseSite: ['electronics-spa']
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en'
+      },
+      features: {
+        level: '2.1'
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
